68ca75e9dfe3a6bee450c8e6ccbb52bbeaca3330
AVA-Sneha <127439304+AVA-Sneha@users.noreply.github.com>
2023-06-11 23:25:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-const serverPort = 3000; 
+const serverPort = 9000; 
 const serverURL = `http://localhost:${serverPort}/`;
 
 class CustomerApp extends Component {
